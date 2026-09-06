cb0e005f300c936c2bb9543f278a268b394a3f70
ryleigh023 <ramyapriyasankar@gmail.com>
2026-09-06 23:04:41 +0530

    Create day02.swift

---
+++ 100-days-of-swift/day02/day02.swift
@@ -0,0 +1,25 @@
+// Day 2: Multi-line Strings, Doubles and Booleans, String Interpolation
+
+// Multi-line strings
+let bigQuote = """
+The world is a
+big place with
+many stories to tell.
+"""
+
+// Doubles (decimal numbers)
+let pi = 3.141
+var currentBalance = 100.0
+currentBalance += 20.5
+
+// Booleans
+var isAuthenticated = true
+isAuthenticated = false
+var isVIP = true
+isVIP.toggle() // flips true/false, now false
+
+// String interpolation
+let userName = "Taylor"
+let age = 26
+let message = "Hello, my name is \(userName) and I'm \(age) years old."
+print(message)
