bd1e80faf20409d9b95053799f228bb912d8d158
ryleigh023 <ramyapriyasankar@gmail.com>
2026-09-06 23:04:18 +0530

    Create day01.swift

---
+++ 100-days-of-swift/day01/day01.swift
@@ -0,0 +1,23 @@
+// Day 1: Variables, Constants, Strings, and Integers
+
+// Variables can be changed after creation
+var name = "Ted"
+name = "Rebecca"
+
+// Constants cannot be changed once set
+let character = "Daphne"
+
+// Strings
+var greeting = "Hello, playground"
+let quote = "Then he tapped a sign that said \"Believe\"."
+
+// Integers
+let score = 10
+let higherScore = score + 10
+let halvedScore = score / 2
+
+// Compound operators
+var counter = 10
+counter += 5   // counter is now 15
+counter -= 3   // counter is now 12
+counter *= 2   // counter is now 24
